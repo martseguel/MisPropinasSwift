b9c25e886fc0556893d01aabc305775bb5c8e957
martseguel <martseguel@alumnos.uai.cl>
2023-11-27 11:35:14 -0300

    Initial Commit

---
+++ MisPropinas/MisPropinasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisPropinasApp.swift
+//  MisPropinas
+//
+//  Created by Martin Seguel on 27-11-23.
+//
+
+import SwiftUI
+
+@main
+struct MisPropinasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
